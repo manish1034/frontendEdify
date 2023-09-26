1d11d18c80ee742d2603f0352644b2177075bc12
manish1034 <76244041+manish1034@users.noreply.github.com>
2023-09-26 14:02:48 +0530

    myfirstchange

--- src/components/ProfileComponent/ProNavbar.jsx
+++ src/components/ProfileComponent/ProNavbar.jsx
@@ -18,7 +18,7 @@ const ProNavbar = () => {
                     <Link to='/settings'>Account Settings</Link>
                 </div>
                 <div className="hover:border-y-[0.1px] hover:border-black/70 w-[90%] p-4 cursor-pointer">
-                    <Link to='/login'>Logout</Link>
+                    <Link to='/logout'>Logout</Link>
                 </div>
             </div>
         </div>
