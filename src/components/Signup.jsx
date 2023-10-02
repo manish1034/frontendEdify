af83b81385234733b12981347f18ba1497638d7b
manish1034 <76244041+manish1034@users.noreply.github.com>
2023-10-02 21:26:46 +0530

    2nd one of the day

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,53 +1,99 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
 import { signup } from "../assets";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 
 const Signup = () => {
-  return (
-    <div className="relative bg-[rgb(242,241,236)] flex flex-row justify-around rounded-b-3xl mb-20 h-[160vh]">
 
-        {/* Left */}
-        <div className="mt-[16%] mr-[5rem] p-8">
-            <img src={signup} alt="" className="w- full h-[100vh] rounded-3xl"></img>
-        </div>
-        
-        {/* right */}
-        <div className="flex flex-col gap-6 mt-[12%] mr-[15%] w-[60vh]">
-            <div className="text-5xl font-mono text-black text-center mb-6">Signup</div>
-            <div className="text-2xl text-black mr-32">Username</div>
-            <input className="border-b-[1px] border-black bg-[rgb(242,241,236)] -mt-3 focus:outline-none" ></input>
-            <div className="text-2xl text-black mr-32">My Name is</div>
-            <input className="border-b-[1px] border-black bg-[rgb(242,241,236)] -mt-3 focus:outline-none" ></input>
-            <div className="text-2xl text-black mr-32">I Study in</div>
-            <select className='w-full bg-transparent border-b-[1px] border-black -mt-3 text-[18px] font-semibold focus:outline-none' defaultValue="">
-                <option value=""></option>
-                <option value="school">school</option>
-                <option value="college">college</option>
-            </select>
-            <div className="text-2xl text-black mr-32">Courses</div>
-            <select className='w-full bg-transparent border-b-[1px] border-black -mt-3 text-[18px] font-semibold focus:outline-none' defaultValue="">
-                <option value=""></option>
-                <option value="school">8th</option>
-                <option value="college">9th</option>
-                <option value="college">10th</option>
-                <option value="college">11th</option>
-                <option value="college">12th</option>
-            </select>
-            <div className="text-2xl text-black mr-32">This is my Email</div>
-            <input className="border-b-[1px] border-black bg-[rgb(242,241,236)] -mt-3 focus:outline-none" ></input>
-            <div className="text-2xl text-black mr-32">Set My Password to</div>
-            <input className="border-b-[1px] border-black bg-[rgb(242,241,236)] -mt-3 focus:outline-none" ></input>
-            <div className="text-2xl text-black mr-32">My Password is</div>
-            <input className="border-b-[1px] border-black bg-[rgb(242,241,236)] -mt-3 focus:outline-none" ></input>
-            <button className="bg-black rounded-lg h-12 text-white">Register</button>    
-            <h1 className="text-black/60 text-center text-lg font-sans">Already a member!?
-                <span className="cursor-pointer text-black/60 text-lg font-sans">
-                    <Link to='/login'> Login</Link>
-                </span>
-            </h1>       
-        </div>
+    const navigate = useNavigate();
+    const [register, setRegister] = useState({
+        username : '',
+        fullname : '',
+        inWhat : '',
+        course : '',
+        email : '',
+        password : '',
+        cPass : ''
+    })
 
-    </div>
-  )
+    const handleRegister = (e) =>{
+        setRegister({...register, [e.target.name]: e.target.value });
+    }
+    
+    const [In,setIn] = useState('')
+    const setCoOrSc= (e) => {
+        let val = e.target.value;
+        setIn(val);
+    }
+
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const {username,fullname,inWhat,course,email,password,cPass} = register;
+        try {
+            const response = await axios.post('/auth/register', {username,fullname,inWhat,course,email,password,cPass});
+            navigate('/login');
+        } catch (error) {
+            console.log("not able to register");
+        }
+      };
+    return (
+        <div className="relative bg-[rgb(242,241,236)] flex flex-row justify-around rounded-b-3xl mb-20 h-[160vh]">
+
+            {/* Left */}
+            <div className="mt-[16%] mr-[5rem] p-8">
+                <img src={signup} alt="" className="w- full h-[100vh] rounded-3xl"></img>
+            </div>
+            
+            {/* right */}
+            <div className="flex flex-col gap-6 mt-[12%] mr-[15%] w-[60vh]">
+                <div className="text-5xl font-mono text-black text-center mb-6">Signup</div>
+                <div className="text-2xl text-black mr-32">Username</div>
+                <input name="username" className="border-b-[1px] border-black bg-[rgb(242,241,236)] -mt-3 focus:outline-none" ></input>
+                <div className="text-2xl text-black mr-32">My Name is</div>
+                <input name="fullname" className="border-b-[1px] border-black bg-[rgb(242,241,236)] -mt-3 focus:outline-none" ></input>
+                <div className="text-2xl text-black mr-32">I Study in</div>
+                <select name="inWhat" onClick={setCoOrSc} value={register.inWhat} onChange={handleRegister} className='w-full bg-transparent border-b-[1px] border-black -mt-3 text-[18px] font-semibold focus:outline-none' defaultValue="">
+                    <option value=""></option>
+                    <option value="school">school</option>
+                    <option value="college">college</option>
+                </select>
+                <div className="text-2xl text-black mr-32">Courses</div>
+                { In === 'school' ? (
+                    <select name="course" value={register.course} onChange={handleRegister} className='w-full bg-transparent border-b-[1px] border-black -mt-3 text-[18px] font-semibold focus:outline-none' defaultValue="">
+                        <option value=""></option>
+                        <option value="6">6th</option>
+                        <option value="7">7th</option>
+                        <option value="8">8th</option>
+                        <option value="9">9th</option>
+                        <option value="10">10th</option>
+                        <option value="11">11th</option>
+                        <option value="12">12th</option>
+                    </select>
+                ) : (
+                    <select name="course" value={register.course} onChange={handleRegister} className='w-full bg-transparent border-b-[1px] border-black -mt-3 text-[18px] font-semibold focus:outline-none' defaultValue="">
+                        <option value=""></option>
+                        <option value="Btech">B.Tech</option>
+                        <option value="BSc">B.Sc</option>
+                    </select>
+                )}
+                <div className="text-2xl text-black mr-32">This is my Email</div>
+                <input type="email" name="email" autoComplete="off" value={register.email} onChange={handleRegister} className="border-b-[1px] border-black bg-[rgb(242,241,236)] -mt-3 focus:outline-none" ></input>
+                <div className="text-2xl text-black mr-32">Set My Password to</div>
+                <input type="password" name="password" autoComplete="off" value={register.password} onChange={handleRegister} className="border-b-[1px] border-black bg-[rgb(242,241,236)] -mt-3 focus:outline-none" ></input>
+                <div className="text-2xl text-black mr-32">My Password is</div>
+                <input type="password" name="cPass" autoComplete="off" value={register.cPass} onChange={handleRegister} className="border-b-[1px] border-black bg-[rgb(242,241,236)] -mt-3 focus:outline-none" ></input>
+                <button onClick={handleSubmit} className="bg-black rounded-lg h-12 text-white">Register</button>    
+                <h1 className="text-black/60 text-center text-lg font-sans">Already a member!?
+                    <span className="cursor-pointer text-black/60 text-lg font-sans">
+                        <Link to='/login'> Login</Link>
+                    </span>
+                </h1>       
+            </div>
+
+        </div>
+    )
 }
 
 export default Signup;
--- src/components/ViewCourse.jsx
+++ src/components/ViewCourse.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable react/prop-types */
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 // import { useLocation } from "react-router-dom";
+import { TextField } from "@mui/material/";
 import axios from "axios";
 
 function Accordion(props) {
@@ -63,29 +65,118 @@ const ViewCourse = () => {
   //     item[0].subjects.map((item) => item.videos.map((item) => item))
   //   )
   // );
+//   const SchoolCourse = () => {
+//     return (
+//       <div>
+//         <div className=" relative my-20 flex flex-col justify-center">
+//           <h1 className=" text-[20px] font-semibold font-mono">
+//             it is our pleasure to work with you and make your school journey fun
+//             and memorable
+//           </h1>
+//         </div>
+//         <div>
+//           <span className=" text-[20px] font-mono">
+//             here are the courses according to your needs
+//           </span>
+//         </div>
+//         <div className=" flex flex-wrap relative w-full">
+//           {school.length > 0 ? (
+//             school.map((item) =>
+//               item[0].subjects.map((item, index) => (
+//                 <div
+//                   key={index}
+//                   className=" flex-[3.5] bg-gray-500 mx-3 rounded-[2px] py-3 px-5 cursor-pointer "
+//                 >
+//                   <div>{item.subjectname}</div>
+//                 </div>
+//               ))
+//             )
+//           ) : (
+//             <p>...</p>
+//           )}
+//         </div>
+//         <div className=" flex flex-row justify-between items-center">
+//           <div className="bg-gray-500 mx-3 my-2 rounded-[2px] py-3 px-5 cursor-pointer ">
+//             Notes
+//           </div>
+//           <div className="bg-gray-500 mx-3 my-2 rounded-[2px] py-3 px-5 cursor-pointer ">
+//             Take Tests
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   };
+
+  
   const SchoolCourse = () => {
     return (
       <div>
-        <div className=" relative my-20 flex flex-col justify-center">
+        {/* <div className=" relative my-40 flex flex-col justify-center">
           <h1 className=" text-[20px] font-semibold font-mono">
-            it is our pleasure to work with you and make your school journey fun
-            and memorable
+            it is our pleasure to work with you and make your college journey
+            fun and memorable
           </h1>
-        </div>
-        <div>
-          <span className=" text-[20px] font-mono">
-            here are the courses according to your needs
+        </div> */}
+        <div className="relative mt-[12%]">
+          <span className="font-mono text-start ml-20 text-5xl">
+            Let's take one step forward towards... <br/> 
           </span>
+          <div className="ml-20 p-3 italic mb-10 mt-2 font-mono text-6xl text-[rgb(46,81,167)]">LEARNING!!</div>
         </div>
-        <div className=" flex flex-wrap relative w-full">
+        <div className=" flex flex-col relative w-[90%] left-16 mb-8">
           {school.length > 0 ? (
             school.map((item) =>
               item[0].subjects.map((item, index) => (
                 <div
                   key={index}
-                  className=" flex-[3.5] bg-gray-500 mx-3 rounded-[2px] py-3 px-5 cursor-pointer "
+                  className="flex-[5] bg-[rgb(249,249,245)] mx-3 my-2 p-5 text-black/80 text-xl rounded-sm"
                 >
-                  <div>{item.subjectname}</div>
+                    <Accordion title={item.subjectname}>
+                        {item.videos.map((item, index) => (
+                        <div
+                            key={index}
+                            className="flex bg-white m-2 my-6 p-2 rounded-2xl cursor-pointer text-blue-600"
+                        >
+                            {/* Left-VideoSection */}
+                            <div className="p-4 mr-[5rem] ml-10 rounded-md mb-5 mt-4">
+                                <iframe className="m-0 rounded-lg shadow-black shadow-md"
+                                    width="590"
+                                    height="355"
+                                    src={`https://www.youtube.com/embed/${item.videos}`}
+                                    title="YouTube video player"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowFullScreen
+                                />
+                            </div>
+                            {/* right-NoteSection */}
+                            <div className="relative right-0 mb-5 mt-3">
+                                <div className="border-b-[2px] border-r-[2px] border-grey shadow-xl">
+                                    <TextField
+                                    id="standard-multiline-static"
+                                    label="FOR NOTES"
+                                    InputLabelProps={{sx:{color: "blue-500"}}}
+                                    multiline
+                                    rows={14}
+                                    sx={{width: 450, height: 390, marginLeft: 1}}
+                                    placeholder="Write your own notes...HERE"
+                                    variant="standard"
+                                    />
+                                </div>
+                                <div className="relative left-[15.7rem] bg-black/80 text-white h-[3rem] w-[10rem] cursor-pointer mt-[2.7rem] rounded-md hover:bg-white border-b-[2px] border-r-[2px] shadow-md shadow-grey">
+                                    <h2 className="text-white font-sans p-[0.6rem] pl-9 hover:text-black/90">Save as Pdf</h2>
+                                </div>
+                            </div>
+                        </div>
+                        ))}
+                        <div className=" flex flex-row justify-between items-center">
+                        <div className="bg-white m-2 px-4 py-2 text-lg rounded-lg cursor-pointer text-black/80 font-sans font-normal hover:bg-black/80 hover:text-white/80">
+                            Notes
+                        </div>
+                        <div className="bg-white mr-2 px-4 py-2 text-lg rounded-lg cursor-pointer text-black/80 font-sans font-normal hover:bg-black/80 hover:text-white/80">
+                            Take Tests
+                        </div>
+                        </div>
+                    </Accordion>
                 </div>
               ))
             )
@@ -93,61 +184,88 @@ const ViewCourse = () => {
             <p>...</p>
           )}
         </div>
-        <div className=" flex flex-row justify-between items-center">
-          <div className="bg-gray-500 mx-3 my-2 rounded-[2px] py-3 px-5 cursor-pointer ">
-            Notes
-          </div>
-          <div className="bg-gray-500 mx-3 my-2 rounded-[2px] py-3 px-5 cursor-pointer ">
-            Take Tests
-          </div>
-        </div>
       </div>
     );
   };
 
-  
+
   const CollegeCourse = () => {
     return (
       <div>
-        <div className=" relative my-20 flex flex-col justify-center">
+        {/* <div className=" relative my-40 flex flex-col justify-center">
           <h1 className=" text-[20px] font-semibold font-mono">
             it is our pleasure to work with you and make your college journey
             fun and memorable
           </h1>
-        </div>
-        <div>
-          <span className=" text-[20px] font-mono">
-            here are the courses according to your needs
+        </div> */}
+        <div className="relative mt-[12%]">
+          <span className="font-mono text-start ml-20 text-5xl">
+            Let's take one step forward towards... <br/> 
           </span>
+          <div className="ml-20 p-3 italic mb-10 mt-2 font-mono text-6xl text-[rgb(46,81,167)]">LEARNING!!</div>
         </div>
-        <div className=" flex flex-col relative w-full">
+        <div className=" flex flex-col relative w-[90%] left-16 mb-8">
           {college.length > 0 ? (
             college.map((item) =>
               item[0].subjects.map((item, index) => (
                 <div
                   key={index}
-                  className=" flex-[5] bg-gray-500 mx-3 my-2 rounded-[2px] py-3 px-5 "
+                  className="flex-[5] bg-[rgb(249,249,245)] mx-3 my-2 p-5 text-black/80 text-xl rounded-sm"
                 >
-                  <Accordion title={item.name}>
-                    {item.videos.map((item, index) => (
-                      <div
-                        key={index}
-                        className=" bg-white m-2 p-2 rounded-[2px] cursor-pointer"
-                      >
-                        <a href={item.url}>
-                          <Accordion title={item.title}></Accordion>
-                        </a>
-                      </div>
-                    ))}
-                    <div className=" flex flex-row justify-between items-center">
-                      <div className="bg-white m-2 p-2 rounded-[2px] cursor-pointer">
-                        Notes
-                      </div>
-                      <div className="bg-white m-2 p-2 rounded-[2px] cursor-pointer ">
-                        Take Tests
-                      </div>
-                    </div>
-                  </Accordion>
+                    <Accordion title={item.name}>
+                        {item.videos.map((item, index) => (
+                        <div
+                            key={index}
+                            className="flex bg-white m-2 my-6 p-2 rounded-2xl cursor-pointer text-blue-600"
+                        >
+                            {/* Left-VideoSection */}
+                            <div className="p-4 mr-[5rem] ml-10 rounded-md mb-5 mt-4">
+                                <iframe className="m-0 rounded-lg shadow-black shadow-md"
+                                    width="590"
+                                    height="355"
+                                    src={`https://www.youtube.com/embed/${item.videos}`}
+                                    title="YouTube video player"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowFullScreen
+                                />
+                                {/* <div className="flex flex-row">
+                                    <div className="bg-white h-[3.7rem] w-[10rem] cursor-pointer mt-10 rounded-md hover:bg-[rgb(92,84,241)] border-b-[2px] border-r-[2px] shadow-md shadow-grey">
+                                    <h2 className="text-[rgb(92,84,241)] font-sans font-semibold p-[1rem] pl-9 hover:text-white"><a href="https://www.toppersnotes.com/wp-content/uploads/2017/02/P.pdf" target="_blank">For Notes</a></h2>
+                                    </div>
+                                    <div className="bg-white relative left-[12rem] h-[3.7rem] w-[10rem] cursor-pointer mt-10 ml-4 rounded-md hover:bg-[rgb(92,84,241)] border-b-[2px] border-r-[2px] shadow-md shadow-grey">
+                                    <h2 className="text-[rgb(92,84,241)] font-sans font-semibold p-[1rem] pl-9 hover:text-white"><a href="https://forms.gle/NBE6CRAHGCWYax6d6" target="_blank">Quick Quiz</a></h2>
+                                    </div>
+                                </div> */}
+                            </div>
+                            {/* right-NoteSection */}
+                            <div className="relative right-0 mb-5 mt-3">
+                                <div className="border-b-[2px] border-r-[2px] border-grey shadow-xl">
+                                    <TextField
+                                    id="standard-multiline-static"
+                                    label="FOR NOTES"
+                                    InputLabelProps={{sx:{color: "blue-500"}}}
+                                    multiline
+                                    rows={14}
+                                    sx={{width: 450, height: 390, marginLeft: 1}}
+                                    placeholder="Write your own notes...HERE"
+                                    variant="standard"
+                                    />
+                                </div>
+                                <div className="relative left-[15.7rem] bg-black/80 text-white h-[3rem] w-[10rem] cursor-pointer mt-[2.7rem] rounded-md hover:bg-white border-b-[2px] border-r-[2px] shadow-md shadow-grey">
+                                    <h2 className="text-white font-sans p-[0.6rem] pl-9 hover:text-black/90">Save as Pdf</h2>
+                                </div>
+                            </div>
+                        </div>
+                        ))}
+                        <div className=" flex flex-row justify-between items-center">
+                        <div className="bg-white m-2 px-4 py-2 text-lg rounded-lg cursor-pointer text-black/80 font-sans font-normal hover:bg-black/80 hover:text-white/80">
+                            Notes
+                        </div>
+                        <div className="bg-white mr-2 px-4 py-2 text-lg rounded-lg cursor-pointer text-black/80 font-sans font-normal hover:bg-black/80 hover:text-white/80">
+                            Take Tests
+                        </div>
+                        </div>
+                    </Accordion>
                 </div>
               ))
             )
